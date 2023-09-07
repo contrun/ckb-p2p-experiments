690c26f8f5f52c9618b8425348681659453ad46c
YI <uuuuuu@protonmail.com>
2023-09-08 01:55:57 +0800

    add identified nodes to kademlia

--- p2p-pipe/src/main.rs
+++ p2p-pipe/src/main.rs
@@ -1,25 +1,29 @@
 use futures::{future::Either, prelude::*};
 use libp2p::{
     core::{muxing::StreamMuxerBox, transport::OrTransport, upgrade},
-    dns, gossipsub, identity,
-    kad::{record::store::MemoryStore, GetProvidersOk, Kademlia, KademliaEvent, Mode, QueryResult},
+    dns, gossipsub, identify, identity,
+    kad::{
+        self, record::store::MemoryStore, GetProvidersOk, Kademlia, KademliaEvent, Mode,
+        QueryResult,
+    },
     mdns, noise, quic,
     swarm::NetworkBehaviour,
     swarm::{SwarmBuilder, SwarmEvent},
     tcp, yamux, Multiaddr, PeerId, Transport,
 };
+use log::{debug, info};
 use std::collections::hash_map::DefaultHasher;
 use std::error::Error;
 use std::hash::{Hash, Hasher};
 use std::str::FromStr;
 use std::time::Duration;
 use tokio::{io, time};
-
 use tokio_util::codec::{FramedRead, LinesCodec};
 
 // We create a custom network behaviour that combines Gossipsub and Mdns.
 #[derive(NetworkBehaviour)]
 struct MyBehaviour {
+    identify: identify::Behaviour,
     gossipsub: gossipsub::Behaviour,
     mdns: mdns::tokio::Behaviour,
     kademlia: Kademlia<MemoryStore>,
@@ -27,7 +31,7 @@ struct MyBehaviour {
 
 const GOSSIPSUB_TOPIC_NAME: &str = "test-gossipsub-topic";
 const KADEMLIA_PROVIDE_NAME: &str = "test-kademlia-provides";
-const BOOTNODES: [(&str, &str); 5] = [
+const BOOTNODES: [(&str, &str); 6] = [
     (
         "/dnsaddr/bootstrap.libp2p.io",
         "QmNnooDu7bfjPFoTZYxMNLWUQJyrVwtbZg5gBMjTezGAJN",
@@ -48,13 +52,18 @@ const BOOTNODES: [(&str, &str); 5] = [
         "/ip4/104.131.131.82/tcp/4001",
         "QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ",
     ), // mars.i.ipfs.io
+    (
+        "/ip4/127.0.0.1/tcp/44001",
+        "12D3KooWA8BKExWnva7sQnUHcs2zJ8QK7vMK9dCUfz5oW2A7Qbbh",
+    ),
 ];
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     // Create a random PeerId
     let id_keys = identity::Keypair::generate_ed25519();
-    let local_peer_id = PeerId::from(id_keys.public());
+    let pub_key = id_keys.public();
+    let local_peer_id = PeerId::from(pub_key.clone());
     println!("Local peer id: {local_peer_id}");
 
     let transport = {
@@ -108,6 +117,10 @@ async fn main() -> Result<(), Box<dyn Error>> {
 
     // Create a Swarm to manage peers and events
     let mut swarm = {
+        let identify = identify::Behaviour::new(
+            identify::Config::new("ipfs/0.1.0".to_string(), pub_key)
+                .with_agent_version(format!("rust-libp2p-server/{}", env!("CARGO_PKG_VERSION"))),
+        );
         let mdns = mdns::tokio::Behaviour::new(mdns::Config::default(), local_peer_id)?;
         let mut kademlia = Kademlia::new(local_peer_id, MemoryStore::new(local_peer_id));
         for (addr, id) in &BOOTNODES {
@@ -122,6 +135,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .unwrap();
         kademlia.set_mode(Some(Mode::Server));
         let behaviour = MyBehaviour {
+            identify,
             gossipsub,
             mdns,
             kademlia,
@@ -158,6 +172,24 @@ async fn main() -> Result<(), Box<dyn Error>> {
             event = swarm.select_next_some() => {
                 // dbg!(&event);
                 match event {
+                    SwarmEvent::Behaviour(MyBehaviourEvent::Identify(identify::Event::Received {
+                            peer_id,
+                            info:
+                                identify::Info {
+                                    listen_addrs,
+                                    protocols,
+                                    ..
+                                },
+                        })) => {
+                            if protocols.iter().any(|p| *p == kad::PROTOCOL_NAME) {
+                                for addr in listen_addrs {
+                                    swarm
+                                        .behaviour_mut()
+                                        .kademlia
+                                        .add_address(&peer_id, addr);
+                                }
+                            }
+                    },
                     SwarmEvent::Behaviour(MyBehaviourEvent::Mdns(mdns::Event::Discovered(list))) => {
                         for (peer_id, _multiaddr) in list {
                             println!("mDNS discovered a new peer: {peer_id}");
@@ -193,6 +225,9 @@ async fn main() -> Result<(), Box<dyn Error>> {
                         },
                     )) => {
                         println!("Obatained list of providers: {providers:?}");
+                    },
+                    SwarmEvent::Behaviour(MyBehaviourEvent::Kademlia(_)) => {
+                        // dbg!(&event);
                     },
                         _ => {}
                     }
