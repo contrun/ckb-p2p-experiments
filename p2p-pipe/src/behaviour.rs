6754df60e26e568767e4b13c45fec4b3c942acc5
YI <uuuuuu@protonmail.com>
2023-09-07 22:34:05 +0800

    discover peers via gossipsub and mdns

---
+++ p2p-pipe/src/behaviour.rs
@@ -0,0 +1,8 @@
+use libp2p::{gossipsub, mdns, swarm::NetworkBehaviour};
+
+// We create a custom network behaviour that combines Gossipsub and Mdns.
+#[derive(NetworkBehaviour)]
+struct MyBehaviour {
+    gossipsub: gossipsub::Behaviour,
+    mdns: mdns::async_io::Behaviour,
+}
