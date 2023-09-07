a31f1faa354b7fd388564add1ea55dbab1ec20af
YI <uuuuuu@protonmail.com>
2023-09-07 23:46:25 +0800

    remove lib.rs temporarily

--- p2p-pipe/src/behaviour.rs
+++
@@ -1,8 +0,0 @@
-use libp2p::{gossipsub, mdns, swarm::NetworkBehaviour};
-
-// We create a custom network behaviour that combines Gossipsub and Mdns.
-#[derive(NetworkBehaviour)]
-struct MyBehaviour {
-    gossipsub: gossipsub::Behaviour,
-    mdns: mdns::tokio::Behaviour,
-}
